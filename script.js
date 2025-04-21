19d3408d879b0660d7b9fbc3e3aa565eae985c6d
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-21 20:07:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let form=document.querySeletor("form");
+let nameBox=document.querySeletor("#username").value;
+let passBox=document.querySeletor("#password").value;
+let checkBox=document.querySeletor("#checkbox");
+let Submit=document.querySeletor("#submit");
+
+
+
+form.addEventListener("submit",()=>{
+	let btn=document.querySeletor("#existing");
+	if(checkBox){
+		localStorage.setItem("nameData",JSON.stringify("nameBox"));
+		localStorage.setItem("passData",JSON.stringify("passBox"));
+		btn.style.display="block";
+		btn.addEventListener("click",()=>{
+			alert(`Logged in as ${nameBox}`);
+		})
+	}
+	else{
+		alert(`Logged in as ${nameBox}`);
+	}
+})
