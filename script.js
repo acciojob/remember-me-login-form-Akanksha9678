a8310ccb825d773702b34c983f4ab1509156dd55
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-21 20:11:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ let Submit=document.querySelector("#submit");
 
 
 
-form.addEventListener("submit",()=>{
+form.addEventListener("submit",(e)=>{
+	e.preventDefault();
 	let btn=document.querySelector("#existing");
 	if(checkBox){
-		localStorage.setItem("nameData",JSON.stringify("nameBox"));
-		localStorage.setItem("passData",JSON.stringify("passBox"));
+		localStorage.setItem("nameData",JSON.stringify(nameBox));
+		localStorage.setItem("passData",JSON.stringify(passBox));
 		btn.style.display="block";
 		btn.addEventListener("click",()=>{
 			alert(`Logged in as ${nameBox}`);
